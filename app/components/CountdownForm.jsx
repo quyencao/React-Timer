8bdb0570d1f848b14ee84420ad4e579cb227539f
Quyen <boy20195@gmail.com>
2016-10-06 20:41:49 +0700

    Add CountdownForm Component and Test

---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var CountdownForm = React.createClass({
+  onSubmit: function(e) {
+    e.preventDefault();
+
+    var strSeconds = this.refs.seconds.value;
+
+    // start is number end in number middle is number
+    if(strSeconds.match(/^[0-9]*$/)) {
+      // Clear input
+      this.refs.seconds.value = '';
+
+      // Call a function in parent pass down to change state!!!
+      this.props.onSetCountdown(parseInt(strSeconds, 10));
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <form ref="form" onSubmit={this.onSubmit} className="countdown-form">
+            <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+            <button type="submit" className="button expanded">Start</button>
+        </form>
+      </div>
+    )
+  }
+});
+
+module.exports = CountdownForm;
